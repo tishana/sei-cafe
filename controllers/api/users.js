111f02a0fe236f8991a5493fbf22de6bbc6496af
Tishana <tishana.trainor@gmail.com>
2023-05-24 14:03:42 -0400

    Second commit

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,13 +1,19 @@
+const User = require('../../models/user')
 module.exports = {
     create
   };
   
-  function create(req, res) {
-    // Baby step...
-    res.json({
-      user: {
-        name: req.body.name,
-        email: req.body.email
-      }
-    });
+  async function create(req, res) {
+    try {
+      const user = await User.create(req.body);
+    } catch (err) {
+      res.status(400).json(err)
+    }
+    // // Baby step...
+    // res.json({
+    //   user: {
+    //     name: req.body.name,
+    //     email: req.body.email
+    //   }
+    // });
   }
